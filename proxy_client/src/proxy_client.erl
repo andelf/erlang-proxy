d863ad1a65d4a40ce48eb710949f1b40ea42a28a
Feather Andelf <andelf@gmail.com>
2013-04-08 22:18:35 +0800

    proxy_client: renamed proxy_client_app.erl -> proxy_client.erl

--- proxy_client/src/proxy_client.erl
+++ proxy_client/src/proxy_client.erl
@@ -1,38 +1,16 @@
--module(proxy_client).
+-module(proxy_client_app).
 
--export([start/0]).
+-behaviour(application).
 
--export([accept_loop/1]).
+%% Application callbacks
+-export([start/2, stop/1]).
 
--include("proxy_defs.hrl").
+%% ===================================================================
+%% Application callbacks
+%% ===================================================================
 
+start(_StartType, _StartArgs) ->
+    proxy_client_sup:start_link().
 
-
--define(SOCK_OPTIONS,
-        [binary,
-         {reuseaddr, true},
-         {active, false},
-         {nodelay, true}
-        ]).
-
-
-start() ->
-    ConfFile = filename:join(code:priv_dir(proxy_client), "client.conf"),
-    case file:consult(ConfFile) of
-        {ok, Conf} ->
-            ListenPort = proplists:get_value(listen_port, Conf),
-            ListenIP = proplists:get_value(listen_ip, Conf);
-        {error, _} ->
-            ListenPort = 7070,
-            ListenIP = {127,0,0,1}
-    end,
-    {ok, Socket} = gen_tcp:listen(ListenPort, [{ip, ListenIP} | ?SOCK_OPTIONS]),
-    ?LOG("Porxy client listen on ~p : ~p~n", [ListenIP, ListenPort]),
-    accept_loop(Socket).
-
-
-accept_loop(Socket) ->
-    {ok, Client} = gen_tcp:accept(Socket),
-    {ok, Pid} = supervisor:start_child(proxy_client_worker_sup, [Client]),
-    ok = gen_tcp:controlling_process(Client, Pid),
-    accept_loop(Socket).
+stop(_State) ->
+    ok.
